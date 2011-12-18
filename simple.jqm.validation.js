8d3188b7979575edaa5ad5673daabd28ed3a3af5
Sal Valverde <salomon.valverde@gmail.com>
2011-12-18 11:27:35 -0700

    add url to internal error_dialog page

--- simple.jqm.validation.js
+++ simple.jqm.validation.js
@@ -1,37 +1,32 @@
-// TODO clean up optimize then push to github (public)
-
-
 $('#main').live('pageinit',function(event){		
 
-	if($('#contactus')) {
+  	$('#send').click(function() {
+	    var errors = 0;
+	    $(".simple_validate :input").map(function(){
+	    
+	         if( !$(this).val() ) { errors++; }    
+	    });
+	    
+	    if(errors > 0){  
+			// popup dialog with generic error message
+	        $.mobile.changePage("#error_dialog", {
+				transition: "pop",
+				role: "dialog"
+			});  
+			  	        
+	    } else {    
+	        // load post page
+			$.mobile.changePage("", {   				
+				type: "post", 
+				data: $(".simple_validate").serialize()
+			});
 	
-	  	$('#send').click(function() {
-		    var errors = 0;
-		    $("#contactus :input").map(function(){
-		    
-		         if( !$(this).val() ) { errors++; }    
-		    });
-		    
-		    if(errors > 0){  
-				// popup dialog with generic error message
-		        $.mobile.changePage("#error_dialog", {
-						transition: "pop",
-						role: "dialog"
-					});
-		        
-		    } else {    
-                // changePage to posting code
-				$.mobile.changePage("contact_action.cfm", {
-					type: "post", 
-					data: $("#contactus").serialize()
-				});
-			
-			}
-						
-			// always return false prevent form from posting
-			return false;
+		}  
+					
+		// always return false prevent form from posting
+		return false;
 
-		});
-	}
+	});  
+
 
 });
